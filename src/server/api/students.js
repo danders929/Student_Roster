38bedbadcaa42c3c7eb5b9ee716fb38b1760dc3c
Erin Wolff <erin_wolff@outlook.com>
2023-11-07 11:53:34 -0800

    removed authentication check from students.js

--- src/server/api/students.js
+++ src/server/api/students.js
@@ -1,16 +1,8 @@
 const { ServerError } = require("../errors");
 const prisma = require("../prisma");
-
 const router = require("express").Router();
 module.exports = router;
 
-/** User must be logged in to access students. */
-// router.use((req, res, next) => {
-//   if (!res.locals.user) {
-//     return next(new ServerError(401, "You must be logged in."));
-//   }
-//   next();
-// });
 
 // /api/students - GET all students 
 router.get('/', async (req, res, next) => {
