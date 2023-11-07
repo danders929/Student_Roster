f3900c5ad474d5f2c0c621b66f7ff3259f5d1cba
Dan <sandersda@gmail.com>
2023-11-07 14:02:06 -0600

    Added functionality to seed database using faker

--- src/server/prisma/seed.js
+++ src/server/prisma/seed.js
@@ -1,21 +1,37 @@
-const prisma = require("../prisma");
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const { faker } = require('@faker-js/faker');
 
-/** Seeds the database with a user and some tasks */
-const seed = async () => {
-  await prisma.user.create({
-    data: {
-      username: "foo",
-      password: "bar",
-      tasks: {
-        create: [
-          { description: "task 1" },
-          { description: "task 2" },
-          { description: "task 3" },
-        ],
-      },
-    },
-  });
-};
+
+//**seeds the database with 5 students */
+const seed = async (numStudents =5) => {
+  try {
+    for (let i = 0; i < numStudents; i++) {    
+      const generatedFirstName = faker.person.firstName();
+      const generatedLastName = faker.person.lastName();
+      const generatedEmail = faker.internet.email();
+      const generatedGpa = faker.number.int({ min: 0, max: 4, precision: 0.01 });
+
+      if (!firstName || !lastName || !email) {
+        console.error(`Invalid data for student ${i + 1}. Skipping.`);
+        continue;
+      }
+
+      await prisma.student.create({
+        data: {
+          firstName: generatedFirstName,
+          lastName: generatedLastName,
+          email: generatedEmail,
+          gpa: generatedGpa,
+        },
+      });
+    }
+  } catch (error) {
+    console.error(`Error creating student: ${error}`);
+  }
+}
+
+
 
 seed()
   .then(async () => await prisma.$disconnect())
